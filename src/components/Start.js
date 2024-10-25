3b4dba4eee879b627f3ecd511c0eb91d0fdd7fe6
antoniamstr1 <mestrovic.antonia@gmail.com>
2024-10-25 19:23:05 +0200

    fix start bug

--- src/components/Start.js
+++ src/components/Start.js
@@ -15,42 +15,40 @@ const SignInLogin = () => {
   };
 
   return (
-    <div className="bckg">
-    <div className={`container ${darkMode ? "dark-start" : "light-start"}`}>
-      {/* Top right buttons */}
-      <div className="top-right">
-        {/* Dark Mode Toggle Switch */}
-        <label className="toggle-switch">
-          <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
-          <div className="toggle-switch-background">
-            <div className="toggle-switch-handle"></div>
+    <div className={`bckg ${darkMode ? "dark" : "light"}`}>
+      <div className="container">
+        {/* Top right buttons */}
+        <div className="top-right">
+          {/* Dark Mode Toggle Switch */}
+          <label className="toggle-switch">
+            <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
+            <div className="toggle-switch-background">
+              <div className="toggle-switch-handle"></div>
+            </div>
+          </label>
+
+          <button onClick={toggleLanguage} className="language-btn">
+            <img
+              src="/images/union_jack.png" // Language icon changes with language setting
+              alt="Language Icon"
+              className="language-icon"
+            />
+          </button>
+        </div>
+
+        {/* Center buttons */}
+        <div className="center-start">
+          <h1 className="feelgpt-text">FeelGPT</h1>
+          <div className="button-group">
+            <Link to="/signin">
+              <button className="button-66">Sign In</button>
+            </Link>
+            <Link to="/login">
+              <button className="button-66">Log In</button>
+            </Link>
           </div>
-        </label>
-
-        <button onClick={toggleLanguage} className="language-btn">
-          <img
-            /* TASK: images change with language -> now it is harcoded english */
-            src="/images/union_jack.png"
-            alt="Language Icon"
-            className="language-icon"
-          />
-          {/* {language} */}
-        </button>
-      </div>
-
-      {/* Center buttons */}
-      <div className="center">
-        <h1 className="feelgpt-text">FeelGPT</h1>
-        <div className="button-group">
-          <Link to="/signin">
-            <button className="button-66">Sign In</button>
-          </Link>
-          <Link to="/login">
-            <button className="button-66">Log In</button>
-          </Link>
         </div>
       </div>
-    </div>
     </div>
   );
 };
