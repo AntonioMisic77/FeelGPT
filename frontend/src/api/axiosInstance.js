bf4bc58b99a7b86daf7227507b161ef4763cdc44
pohii <kp51875@fer.hr>
2024-12-23 19:15:42 +0100

    Added cookie

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,19 +1,33 @@
 // src/api/axiosInstance.js
-
 import axios from "axios";
+import Cookies from 'js-cookie';
 
 const API_BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5001/api/v1";
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
+    withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("authToken");
+    async (config) => {
+        let token = localStorage.getItem("authToken") || Cookies.get('authToken');
 
         if (token) {
             config.headers["Authorization"] = `Bearer ${token}`;
+        } else {
+            try {
+                const response = await axiosInstance.get("/verify");
+                if (response.data && response.data.token) {
+                    token = response.data.token;
+                    localStorage.setItem("authToken", token);
+                    Cookies.set('authToken', token, { expires: 10 });
+                    config.headers["Authorization"] = `Bearer ${token}`;
+                }
+            } catch (error) {
+                window.location.href = "/login";
+                return Promise.reject(error);
+            }
         }
 
         return config;
@@ -28,11 +42,17 @@ axiosInstance.interceptors.response.use(
     (error) => {
         const originalRequest = error.config;
 
-        if (error.response &&
+        if (
+            error.response &&
             error.response.status === 401 &&
-            !originalRequest.url.endsWith('login')
+            !originalRequest._retry &&
+            !originalRequest.url.endsWith('/login') &&
+            !originalRequest.url.endsWith('/verify')
         ) {
+            originalRequest._retry = true;
             localStorage.removeItem("authToken");
+            Cookies.remove('authToken');
+
             window.location.href = "/login";
         }
         return Promise.reject(error);
