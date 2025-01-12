0ef12b96800b2246b38222dd2f2b40ebf9a3aa54
AntonioMisic77 <antonio.misic77@outlook.com>
2025-01-12 20:16:10 +0100

    fixinf notification email bug

--- backend/src/api/notification/routine/agenda.ts
+++ backend/src/api/notification/routine/agenda.ts
@@ -29,7 +29,9 @@ agenda.define("send email reminder", async (job: Job) => {
 
   const conversationSummary = await generateSessionSummary(email);
 
-  const html = template({ username, conversationSummary });
+  const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
+
+  const html = template({ username, conversationSummary, frontendUrl });
   await sendMail(email, "Your Conversation Reminder", html);
 });
 
